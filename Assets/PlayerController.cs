25160872e09725d1ac41dea0712fe5b0aead532a
bram <bramdenhond@gmail.com>
2026-03-20 09:07:44 +0100

    Clean up code

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -5,21 +5,19 @@ public class PlayerController : MonoBehaviour
     [Header("Camera & Rotation")]
     public Camera playerCamera;
     public float mouseSensitivity = 100f;
-
-    public float xRotationMultiplier = 10f;
+    public float xRotationMultiplier = 1f;
 
     [Header("Movement")]
     public float moveSpeed = 5f;
-
     public float jumpForce = 5f;
 
     private Rigidbody rb;
     private float xRotation = 0f;
     private bool grounded;
 
-
-    private float stamina = 1.0f;
-    private float maxStamina = 1.0f;
+    [Header("Stamina & Running")]
+    private float stamina = 1f;
+    private float maxStamina = 1f;
     private bool shouldRun = false;
 
     void Start()
@@ -28,26 +26,42 @@ public class PlayerController : MonoBehaviour
         Cursor.lockState = CursorLockMode.Locked;
     }
 
-    void HandleStamina() {
-        if (stamina > 0f && Input.GetKeyDown(KeyCode.LeftShift))
-        {
-            shouldRun = true;
-        }
+    void Update()
+    {
+        HandleStamina();
+        HandleRotation();
+        HandleJump();
+    }
 
-        if (shouldRun) {
+    void FixedUpdate()
+    {
+        HandleWalking();
+    }
+
+    void HandleStamina()
+    {
+        if (stamina > 0f && Input.GetKeyDown(KeyCode.LeftShift))
+            shouldRun = true;
+
+        if (shouldRun)
+        {
             stamina -= Time.deltaTime;
-            if (stamina < 0f) {
+            if (stamina <= 0f)
+            {
                 stamina = 0f;
                 shouldRun = false;
             }
-        } else {
+        }
+        else
+        {
             stamina += Time.deltaTime;
-            if (stamina > maxStamina) stamina = maxStamina;
+            if (stamina > maxStamina)
+                stamina = maxStamina;
         }
     }
 
-    void HandleRotation() {
-        // Mouse look
+    void HandleRotation()
+    {
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime * xRotationMultiplier;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
@@ -60,38 +74,21 @@ public class PlayerController : MonoBehaviour
 
     void HandleJump()
     {
-        // Jump
         if (Input.GetKeyDown(KeyCode.Space) && grounded)
         {
-            rb.linearVelocity = new Vector3(rb.linearVelocity.x, jumpForce, rb.linearVelocity.z);
-            grounded = false; // voorkom dubbel springen
+            rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
+            grounded = false;
         }
     }
 
     void HandleWalking()
     {
-        // Movement
         float moveX = Input.GetAxis("Horizontal");
         float moveZ = Input.GetAxis("Vertical");
 
-        float baseSpeed = 1.0f;
-        if (shouldRun) baseSpeed = 2.0f;
-
+        float currentSpeed = shouldRun ? 2f : 1f;
         Vector3 move = transform.right * moveX + transform.forward * moveZ;
-        rb.MovePosition(rb.position + move * moveSpeed * Time.fixedDeltaTime * baseSpeed);
-    }
-
-
-    void Update()
-    {
-        HandleStamina();
-        HandleRotation();
-        HandleJump();                
-    }
-
-    void FixedUpdate()
-    {
-        HandleWalking();
+        rb.MovePosition(rb.position + move * moveSpeed * Time.fixedDeltaTime * currentSpeed);
     }
 
     void OnCollisionStay(Collision collision)
@@ -99,10 +96,7 @@ public class PlayerController : MonoBehaviour
         foreach (ContactPoint contact in collision.contacts)
         {
             if (Vector3.Dot(contact.normal, Vector3.up) > 0.5f)
-            {
                 grounded = true;
-            }
         }
-    }    
+    }
 }
-
